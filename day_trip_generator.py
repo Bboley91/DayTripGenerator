c4028dd905cd21999c502a37492172f77a9b76e9
Brendann Michael Boley <brendannm.boley91@gmail.com>
2022-07-17 17:38:24 -0400

    Added destination choice loop.

--- day_trip_generator.py
+++ day_trip_generator.py
@@ -1,16 +1,18 @@
 import random
-Destination_list = ['Chicago_Illinois', 'VirginiaBeach_Virginia', 'Kalamazoo_Michigan']
-Random_destination = random.choice(Destination_list)
+Destination_list = ['Chicago_Illinois', 'Virginia Beach_Virginia', 'Kalamazoo_Michigan']
 Transportation_list = ['By plane', 'By bus' 'By train']
-Random_transportation = random.choice(Transportation_list)
 Restaurants_list = ['Burger King', 'Taco Bell', 'Jimmy Johns']
-Random_restaurant = random.choice(Restaurants_list)
 Entertainment_list = ['A museum', 'A car show', 'A concert']
-Random_entertainment = random.choice(Entertainment_list)
-
 
 welcome_greeting = 'Hello, I am your day trip advisor.' 'I am here to help make your vacation plans go as smoothly as possible.'
 print(welcome_greeting)
-print('')
-print('The destination we have picked for you is', Random_destination, '.', 'Does that sound good for you?')
+print('The destination we have picked for you is',random.choice(Destination_list), '.', 'Does that sound good for you?')
+users_choice = input('yes/no' ' ') 
+while users_choice == 'no' or users_choice == 'No' or users_choice == 'NO':
+    print('ok, fortunatly we have other locations available.', 'How about',random.choice(Destination_list), 'instead?')
+    users_choice = input('yes/no' ' ')
+    if users_choice == 'yes' or users_choice == 'Yes' or users_choice == 'YES':
+        print('I am glad i could make the right choice for you!')
+
+
 
